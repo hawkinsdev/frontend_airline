5e480749460c521effb29dc460a366502b9ebb38
Jesus Manuel Varela Miranda <57116065+hawkinsdev@users.noreply.github.com>
2024-11-12 10:02:25 -0500

    feat: agregando componente de spinner para saber cuando se esta ejecutando la peticion

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, { useState } from 'react';
-import { Container, Typography } from '@mui/material';
+import {Box, Container, Typography } from '@mui/material';
 import ItinerarySearch from './components/ItinerarySearch';
 import ItineraryResults from './components/ItineraryResults';
+import CircularProgress from '@mui/material/CircularProgress';
 import { searchItineraries } from './services/api';
 
 const App = () => {
   const [itineraries, setItineraries] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleSearch = async (origin, destination) => {
+    setIsLoading(true);
     const results = await searchItineraries(origin, destination);
     setItineraries(results);
+    setIsLoading(false);
   };
 
   return (
@@ -18,7 +22,10 @@ const App = () => {
         Airline Itinerary Search
       </Typography>
       <ItinerarySearch onSearch={handleSearch} />
-      <ItineraryResults itineraries={itineraries} />
+      {isLoading ?  <Box sx={{display: 'flex', justifyItems: 'center', alignItems: 'center', height: 150}} >
+        <CircularProgress sx={{mx: 'auto'}} /> 
+      </Box> 
+      : <ItineraryResults itineraries={itineraries} />}
     </Container>
   );
 };
